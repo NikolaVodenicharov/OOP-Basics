0f74a71ecc9d9696823eed9902b61cd8b8d0c74a
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-07-15 17:44:29 +0300

    Add last exercises from encapsulation.

---
+++ Encapsulation/FootballTeamGenerator/FootballTeam.cs
@@ -0,0 +1,65 @@
+﻿namespace FootballTeamGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FootballTeam
+    {
+        private string name;
+        private List<Player> players;
+
+        public FootballTeam(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                bool isInvalid = string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value);
+                if (isInvalid)
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            this.players.Add(player);
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            bool validPlayer = this.players.Any(x => x.Name == playerName);
+            if (!validPlayer)
+            {
+                throw new ArgumentException($"Player {playerName} is not in {this.name} team.");
+            }
+
+            var item = this.players.Single(x => x.Name == playerName);
+            this.players.Remove(item);
+        }
+
+        public double CalculateRating()
+        {
+            double rating = 0;
+            if (this.players.Count > 0)
+            {
+                rating += this.players.Average(x => x.CalculateSkillLevel());
+            }
+            
+            return rating;
+        }
+    }
+}
---
+++ Encapsulation/FootballTeamGenerator/FootballTeamGeneratorExecution.cs
@@ -0,0 +1,86 @@
+﻿namespace FootballTeamGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class FootballTeamGeneratorExecution
+    {
+        static void Main()
+        {
+            var teams = new List<FootballTeam>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine().Split(';');
+
+                bool stopLoop = inputLine[0].Equals("end", StringComparison.OrdinalIgnoreCase);
+                if (stopLoop)
+                {
+                    break;
+                }
+
+                try
+                {
+                    OperateInput(teams, inputLine);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void OperateInput(List<FootballTeam> teams, string[] inputLine)
+        {
+            if (inputLine[0].Equals("team", StringComparison.OrdinalIgnoreCase))
+            {
+                teams.Add(new FootballTeam(inputLine[1]));
+            }
+            else if (inputLine[0].Equals("add", StringComparison.OrdinalIgnoreCase))
+            {
+                CheckTeamExistence(teams, inputLine);
+
+                Player player = CreatePlayer(inputLine);
+                teams.Single(x => x.Name == inputLine[1]).AddPlayer(player);
+            }
+            else if (inputLine[0].Equals("remove", StringComparison.OrdinalIgnoreCase))
+            {
+                CheckTeamExistence(teams, inputLine);
+
+                teams.Single(x => x.Name == inputLine[1]).RemovePlayer(inputLine[2]);
+            }
+            else if (inputLine[0].Equals("rating", StringComparison.OrdinalIgnoreCase))
+            {
+                CheckTeamExistence(teams, inputLine);
+
+                double rating = teams.Single(x => x.Name == inputLine[1]).CalculateRating();
+                double roundedRating = Math.Round(rating);
+                Console.WriteLine($"{inputLine[1]} - {roundedRating}");
+            }
+        }
+
+        private static Player CreatePlayer(string[] inputLine)
+        {
+            string name = inputLine[2];
+            double endurance = double.Parse(inputLine[3]);
+            double sprint = double.Parse(inputLine[4]);
+            double dribble = double.Parse(inputLine[5]);
+            double passing = double.Parse(inputLine[6]);
+            double shooting = double.Parse(inputLine[7]);
+
+            Player player = new Player(name, endurance, sprint, dribble, passing, shooting);
+
+            return player;
+        }
+
+        private static void CheckTeamExistence(List<FootballTeam> teams, string[] inputLine)
+        {
+            bool existingTeam = teams.Any(x => x.Name == inputLine[1]);
+            if (!existingTeam)
+            {
+                throw new ArgumentException($"Team {inputLine[1]} does not exist.");
+            }
+        }
+    }
+}
