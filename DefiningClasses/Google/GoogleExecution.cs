ef766efabae03405aea24b5c87bb5d9f62972c3f
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-07-20 01:18:23 +0300

    Add 1 exercises.

---
+++ DefiningClasses/Google/GoogleExecution.cs
@@ -0,0 +1,66 @@
+﻿namespace Google
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GoogleExecution
+    {
+        public static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+                bool stopLoop = inputLine.Equals("End", StringComparison.OrdinalIgnoreCase);
+                if (stopLoop)
+                {
+                    break;
+                }
+
+                var splitLine = inputLine.Split();
+                var name = splitLine[0];
+
+                if (!people.Any(x => x.name == name))
+                {
+                    people.Add(new Person(name));
+                }
+
+                var person = people.Single(x => x.name == name);
+                var command = splitLine[1];
+                var skipTwo = string.Join(" ", splitLine.Skip(2));
+
+                if (command.Equals("company", StringComparison.OrdinalIgnoreCase))
+                {
+                    var salary = double.Parse(splitLine[4]);
+                    string stringedSalary = $"{salary:f2}";
+                    splitLine[4] = stringedSalary;
+                    person.company = string.Join(" ", splitLine.Skip(2));
+                }
+                else if (command.Equals("car", StringComparison.OrdinalIgnoreCase))
+                {
+                    person.car = skipTwo;
+                }
+                else if (command.Equals("pokemon", StringComparison.OrdinalIgnoreCase))
+                {
+                    person.pokemons.Add(skipTwo);
+                }
+                else if (command.Equals("parents", StringComparison.OrdinalIgnoreCase))
+                {
+                    person.parents.Add(skipTwo);
+                }
+                else if (command.Equals("children", StringComparison.OrdinalIgnoreCase))
+                {
+                    person.children.Add(skipTwo);
+                }
+            }
+
+            var serchedPerson = Console.ReadLine();
+            if (people.Any(x => x.name == serchedPerson))
+            {
+                Console.WriteLine(people.Single(x => x.name == serchedPerson));
+            }
+        }
+    }
+}
---
+++ DefiningClasses/Google/Person.cs
@@ -0,0 +1,63 @@
+﻿namespace Google
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        public string name;
+        public string company;
+        public string car;
+        public List<string> pokemons;
+        public List<string> children;
+        public List<string> parents;
+
+        public Person(string name)
+        {
+            this.name = name;
+            this.children = new List<string>();
+            this.parents = new List<string>();
+            this.pokemons = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(this.name);
+
+            sb.AppendLine("Company:");
+            if (this.company != null)
+            {
+                sb.AppendLine(this.company);
+            }
+
+            sb.AppendLine("Car:");
+            if (this.car != null)
+            {
+                sb.AppendLine(this.car);
+            }
+
+            sb.AppendLine("Pokemon:");
+            if (this.pokemons.Count > 0)
+            {
+                sb.AppendLine(string.Join(Environment.NewLine, this.pokemons));
+            }
+
+            sb.AppendLine("Parents:");
+            if (this.parents.Count > 0)
+            {
+                sb.AppendLine(string.Join(Environment.NewLine, this.parents));
+            }
+
+            sb.AppendLine("Children:");
+            if (this.children.Count > 0)
+            {
+                sb.Append(string.Join(Environment.NewLine, this.children));
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
